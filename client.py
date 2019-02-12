5bb0e572e2d7572f76a935fcee32e18d4e703aca
Xinfa Zhu <xinfazhu@gmail.com>
2019-02-11 20:34:28 -0600

    Set TCP and UDP sockets

--- client.py
+++ client.py
@@ -14,6 +14,29 @@ class Chatter:
         self.screen_name = screen_name
         self.host_name = host_name
         self.tcp_port = tcp_port
+        self.set_tcp_socket()
+        self.set_udp_socket()
+
+    def set_tcp_socket(self):
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Bind the socket to the port
+        server_address = (self.host_name, self.tcp_port)
+        #print('connecting to {} port {}'.format(*server_address))
+        sock.connect(server_address)
+        self.tcp_socket = sock
+
+    def set_udp_socket(self, port=0):
+        # port=0 tells the OS to pick a port
+        # Create a UDP/IP socket -> DGRAM
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Bind the socket to a port of OS's choosing
+        server_address = (self.host_name, port)
+        sock.bind(server_address)
+        # To find what port the OS picked, call getsockname()
+        self.udp_socket = sock
+        self.udp_port = sock.getsockname()[1]
+        print('My UDP port is: {}'.format(self.udp_port))
 
 
 def main():
@@ -29,7 +52,7 @@ def main():
     tcp_port = args.tcp_port
     
     chatter = Chatter(screen_name, host_name, tcp_port)
-    print("chatter name: {}".format(chatter.screen_name))
+    #print("chatter name: {}".format(chatter.screen_name))
 
 
 if __name__ == '__main__':
